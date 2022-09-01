84c55ce93cb6c83c6406e458955b6a7dd3622860
挨踢方丈 <itabbot@qq.com>
2022-09-02 00:07:42 +0800

    生成跟踪标识。

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 const Koa = require('koa');
+const rTracer = require('cls-rtracer');
 const router = require('./router');
 
 // 实例化一个 Koa 应用
 const app = new Koa();
+
+// 生成跟踪标识
+app.use(rTracer.koaMiddleware());
 
 // 设置路由
 app.use(router.routes());
