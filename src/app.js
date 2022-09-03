e0837ccf678ad9bae707357101507c141c19e953
挨踢方丈 <itabbot@qq.com>
2022-09-03 22:47:21 +0800

    记录访问日志。

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const Koa = require('koa');
 const rTracer = require('cls-rtracer');
 const router = require('./router');
+const accessLog = require('./libraries/access-log');
 const { handleRouteErrors } = require('./libraries/error-handling');
 
 // 实例化一个 Koa 应用
@@ -14,6 +15,9 @@ app.use(rTracer.koaMiddleware());
 
 // 处理错误（有追踪标识）
 app.use(handleRouteErrors);
+
+// 记录访问日志
+app.use(accessLog);
 
 // 设置路由
 app.use(router.routes());
