197f5bfcbb00305fc48dee6ffe2449550ef0eac8
挨踢方丈 <itabbot@qq.com>
2022-09-07 21:58:21 +0800

    添加 crypto 配置。

--- src/libraries/configuration/schema.js
+++ src/libraries/configuration/schema.js
@@ -1,11 +1,18 @@
 const Joi = require('joi');
 
 module.exports = Joi.object({
-  db: Joi.object({
-    host: Joi.string().required(),
-    port: Joi.number().required(),
-    username: Joi.string().required(),
-    password: Joi.string().required(),
-    database: Joi.string().required(),
-  }),
+  db: Joi.object()
+    .keys({
+      host: Joi.string().required(),
+      port: Joi.number().required(),
+      username: Joi.string().required(),
+      password: Joi.string().required(),
+      database: Joi.string().required(),
+    })
+    .required(),
+  crypto: Joi.object()
+    .keys({
+      aesKey: Joi.string().length(32).required(),
+    })
+    .required(),
 });
