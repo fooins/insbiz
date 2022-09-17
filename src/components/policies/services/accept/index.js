d2e4c6abe6df34c6141fd6a286ab045dfc990e5d
挨踢方丈 <itabbot@qq.com>
2022-09-17 10:43:19 +0800

    调整上下文变量。

--- src/components/policies/services/accept/index.js
+++ src/components/policies/services/accept/index.js
@@ -5,14 +5,11 @@ const basalSchema = require('./schemas/basal');
 
 /**
  * 执行基本校验
+ * @param {object} ctx 上下文对象
  * @param {object} reqData 请求数据
  * @param {object} profile 身份数据
- * @returns {object} 上下文
  */
-const basalValidation = async (reqData, profile) => {
-  // 上下文对象
-  const context = {};
-
+const basalValidation = async (ctx, reqData, profile) => {
   // 字段校验
   const { error, value } = Joi.object(basalSchema).validate({
     orderNo: reqData.orderNo || undefined,
@@ -30,7 +27,7 @@ const basalValidation = async (reqData, profile) => {
       cause: error,
     });
   } else {
-    context.policyData = {
+    ctx.policyData = {
       orderNo: value.orderNo,
       contractCode: value.contractCode,
       contractVersion: value.contractVersion,
@@ -41,11 +38,11 @@ const basalValidation = async (reqData, profile) => {
   // 查询销售渠道
   const producer = await dao.getProducerByCode(profile.producer.code);
   if (!producer) throw error500('获取销售渠道信息失败');
-  context.producer = producer;
+  ctx.producer = producer;
 
   // 检查订单号
   const exists = await dao.getPolicyByOrderNo(
-    context.policyData.orderNo,
+    ctx.policyData.orderNo,
     producer.id,
     {
       attributes: ['id'],
@@ -55,33 +52,31 @@ const basalValidation = async (reqData, profile) => {
 
   // 检查契约
   const contract = await dao.getContractByCode(
-    context.policyData.contractCode,
-    context.policyData.contractVersion,
+    ctx.policyData.contractCode,
+    ctx.policyData.contractVersion,
     { includeProduct: true },
   );
   if (!contract) {
     throw error400('契约不存在');
   } else {
-    context.contract = contract;
-    context.product = contract.Product;
+    ctx.contract = contract;
+    ctx.product = contract.Product;
   }
   if (contract.producerId !== producer.id) throw error400('契约不属于当前渠道');
 
   // 检查计划
   const plan = await dao.getPlanByCode(
-    context.policyData.planCode,
-    context.product.version,
+    ctx.policyData.planCode,
+    ctx.product.version,
   );
   if (!plan) {
     throw error400('保险产品计划不存在');
   } else {
-    context.plan = plan;
+    ctx.plan = plan;
   }
-  if (plan.productId !== context.product.id) {
+  if (plan.productId !== ctx.product.id) {
     throw error400('计划不属于当前保险产品');
   }
-
-  return context;
 };
 
 const bizValidation = async () => {};
@@ -97,23 +92,26 @@ const assembleResponseData = async () => {};
  * @returns {object} 响应的数据
  */
 const acceptInsurance = async (reqData, profile) => {
+  // 定义一个上下文变量
+  const ctx = {};
+
   // 基础校验
-  const context = await basalValidation(reqData, profile);
+  await basalValidation(ctx, reqData, profile);
 
   // 业务规则校验
-  await bizValidation(context, reqData);
+  await bizValidation(ctx, reqData);
 
   // 计费
-  await charging(context);
+  await charging(ctx);
 
   // 生成保单号
-  await generatePolicyNumber(context);
+  await generatePolicyNumber(ctx);
 
   // 保存数据
-  await savePolicyData(context);
+  await savePolicyData(ctx);
 
   // 组装响应数据
-  const responseData = await assembleResponseData(context);
+  const responseData = await assembleResponseData(ctx);
 
   return responseData;
 };
