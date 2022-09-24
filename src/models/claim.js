7e87bd5f392a29a386bd4b3cb2069b161423e222
挨踢方丈 <itabbot@qq.com>
2022-09-24 23:00:27 +0800

    添加理赔单模型。

---
+++ src/models/claim.js
@@ -0,0 +1,36 @@
+const { DataTypes } = require('sequelize');
+const { getDbConnection } = require('../libraries/data-access');
+
+module.exports = function getClaimModel() {
+  return getDbConnection().define(
+    'Claim',
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        comment: '自增ID',
+      },
+      policyId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        comment: '保单ID',
+      },
+      claimNo: {
+        type: DataTypes.STRING(64),
+        allowNull: false,
+        unique: true,
+        comment: '理赔单号',
+      },
+      status: {
+        type: DataTypes.ENUM('pending', 'handing', 'declined', 'confirmed'),
+        defaultValue: 'pending',
+        allowNull: false,
+        comment: '状态',
+      },
+    },
+    {
+      comment: '理赔单表',
+    },
+  );
+};
