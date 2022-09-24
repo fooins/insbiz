f55ccd43b6a83ff3a85aec0dc1ebe1183ceb19c1
挨踢方丈 <itabbot@qq.com>
2022-09-24 19:05:33 +0800

    添加退保路由。

--- src/components/policies/services/index.js
+++ src/components/policies/services/index.js
@@ -2,10 +2,12 @@ const accept = require('./accept');
 const quote = require('./quote');
 const get = require('./get');
 const renew = require('./renew');
+const cancel = require('./cancel');
 
 module.exports = {
   accept,
   quote,
   get,
   renew,
+  cancel,
 };
