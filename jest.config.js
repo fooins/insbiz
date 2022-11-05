fb6306e13f4729f7cf44ef09790d48abc3a98df4
挨踢方丈 <itabbot@qq.com>
2022-11-05 16:13:41 +0800

    测试承保接口。

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   // 检测测试文件的 glob 模式。
   testMatch: ['**/test/api/**/*.js'],
   // 指出是否收集测试时的覆盖率信息。
-  collectCoverage: true,
+  collectCoverage: false,
   // 指示应使用哪个提供程序来检测代码覆盖率。
   coverageProvider: 'v8',
 };
