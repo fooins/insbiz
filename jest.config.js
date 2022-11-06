594ee5be04eff37605fd149e09cba7d65b2907d8
挨踢方丈 <itabbot@qq.com>
2022-11-06 11:46:36 +0800

    申请理赔接口测试。

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   // 检测测试文件的 glob 模式。
   testMatch: ['**/test/api/**/*.js'],
   // 指示是否应在运行期间报告每个单独的测试
-  verbose: true,
+  verbose: false,
   // 指出是否收集测试时的覆盖率信息。
   collectCoverage: false,
   // 指示应使用哪个提供程序来检测代码覆盖率。
